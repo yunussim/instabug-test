8fe1647f758681c1de2239ec0af39d0974f003c1
Yunus Simulya <yunussimulya@gmail.com>
2024-05-17 12:58:20 +0700

    feat: instabug initialize

--- app/src/main/java/com/usetada/instabugtest/InsApp.kt
+++ app/src/main/java/com/usetada/instabugtest/InsApp.kt
@@ -1,4 +1,21 @@
 package com.usetada.instabugtest
 
-class InsApp {
+import android.app.Application
+import com.instabug.library.Instabug
+import com.instabug.library.LogLevel
+import com.instabug.library.invocation.InstabugInvocationEvent
+import com.instabug.library.ui.onboarding.WelcomeMessage
+
+class InsApp : Application() {
+
+    val instabugKey = ""
+
+    override fun onCreate() {
+        super.onCreate()
+        Instabug.Builder(this, instabugKey)
+            .setInvocationEvents(InstabugInvocationEvent.TWO_FINGER_SWIPE_LEFT)
+            .setSdkDebugLogsLevel(LogLevel.DEBUG)
+            .build()
+        Instabug.setWelcomeMessageState(WelcomeMessage.State.DISABLED)
+    }
 }
